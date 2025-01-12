041eeb41c1d512c23333626fe32430999c76e79d
Henry Desroches <yo@henry.codes>
2025-01-12 15:19:24 -0700

    🦋 - Add bluesky ref, update to bun, swap to "henry(from online)"

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,18 @@
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy({ './src/assets/img': 'img' });
   eleventyConfig.addPassthroughCopy({ './src/assets/fonts': 'fonts' });
+
+  eleventyConfig.addFilter('formatDate', (dateStr) => {
+    const date = new Date(dateStr);
+    const timezoneDiff = date.getTimezoneOffset() * 60000;
+    const adjustedDate = new Date(date.valueOf() + timezoneDiff);
+    return adjustedDate.toLocaleDateString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+  });
+  
   return {
     dir: {
       input: 'src',
